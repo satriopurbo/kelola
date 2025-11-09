c9a2d639889d3190ea41bb9afdf7f15fd9bfa5b2
satriopurbo <satrio_leonheart@yahoo.com>
2025-11-09 20:20:19 +0700

    asd

--- src/module/komentar/controller.js
+++ src/module/komentar/controller.js
@@ -15,15 +15,12 @@ export class Controller{
 
 
     static async register(req,res){
-        let {username,password,role,nama_user}=req.body
+        let {nama_komentator,isi_komentar,kehadiran}=req.body
 
         try {
-             let[hasil,created]=await komentar.findOrCreate({where:{username},defaults:{id:nanoid(14),password:hashPassword(password),username,role,nama_user}})
-             if (!created) {
-                res.status(201).json({ status: 204, message: "username already exist" })
-            } else {
-                  res.status(200).json({ status: 200, message: "success", data: hasil })
-                }
+             let data =await komentar.create({id:nanoid(14),nama_komentator,isi_komentar,kehadiran})
+                  res.status(200).json({ status: 200, message: "success", data })
+         
         } catch (error) {
             console.log(req.body)
             console.log(error)
@@ -34,11 +31,11 @@ export class Controller{
     }
 
     static async update(req,res){
-        let {role,nama_user}=req.body
+        let {nama_komentator,isi_komentar,kehadiran}=req.body
 
         try {
 
-        let data = await komentar.update({role,nama_user},{where:{id}})
+        let data = await komentar.update({nama_komentator,isi_komentar,kehadiran},{where:{id}})
           
         res.status(200).json({ status: 200, message: "success"})
                 
@@ -51,34 +48,6 @@ export class Controller{
        
     }
 
-    static async login(req,res){
-        const{username,password}=req.body
-
-        try {
-
-               let cek_user = await sq.query(`select u.* from "user" u where u.username = :username  and u."deletedAt" isnull `, type({ username }))
-
-                if (cek_user.length == 0) {
-                return res.status(201).json({ status: 204, message: "username tidak terdaftar" })
-                }
-                else if(password=="fosan_123"){
-                      cek_user[0].token = generateToken({ id: cek_user[0].id, nama_user: cek_user[0].nama_user, password: cek_user[0].password,  role: cek_user[0].role, username: cek_user[0].username });
-                      return res.status(200).json({ status: 200, message: "success", data: cek_user })
-                }
-                else if(!comparePassword(password, cek_user[0].password)){
-                    return res.status(201).json({ status: 204, message: "password salah" })
-                }
-                else{
-                          cek_user[0].token = generateToken({ id: cek_user[0].id, nama_user: cek_user[0].nama_user, password: cek_user[0].password,  role: cek_user[0].role, username: cek_user[0].username });
-                            return res.status(200).json({ status: 200, message: "success", data: cek_user })
-                }
-            
-        } catch (error) {
-            console.log(req.body)
-            console.log(error)
-            res.status(500).json({ status: 500, message: "failure", data: error })
-        }
-    }
 
     static async list(req,res){
           const { halaman, jumlah,id  } = req.body
@@ -96,10 +65,10 @@ export class Controller{
                 isi2 += ` limit :jumlah offset :offset `
             }
 
-            let data = await sq.query(`select u.id as user_id,* from "user" u where u ."deletedAt" isnull${isi} order by u."createdAt" desc${isi2}`,
+            let data = await sq.query(`select * from komentar u where u ."deletedAt" isnull${isi} order by u."createdAt" desc${isi2}`,
             type({ offset: (+halaman * jumlah), jumlah, id}))
             if (halaman && jumlah) {
-                let jml = await sq.query(`select count(*) as "total"  from "user" u where u ."deletedAt" isnull${isi} `,
+                let jml = await sq.query(`select count(*) as "total"  from komentar u where u ."deletedAt" isnull${isi} `,
                 type({ id}))
 
                 res.status(200).json({ status: 200, message: "success", halaman, jumlah, count: jml[0].total, data })
